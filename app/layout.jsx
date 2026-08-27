ee58e3a84850ce22f1e64b19d6195257d19d856b
Nathan Nicollette <nicollettenathan@gmail.com>
2026-08-27 12:24:17 -0300

    Add app layout

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Vanessa Castro Beauty Hair',
+  description: 'Especialista em loiros, mechas e alisamentos em São Carlos - SP.'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
